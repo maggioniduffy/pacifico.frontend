978c1484d231466f8e1f8d0f3bb12e4bd6ae858d
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2023-01-09 23:38:41 -0300

    nextauth added, not working fine

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4919757fc265af7a"; }
+/******/ 		__webpack_require__.h = function() { return "8c4edf5fe64e1e66"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
