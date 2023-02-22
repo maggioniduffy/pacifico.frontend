61e1ce567e10dde190d1536e136811247999f133
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2023-02-21 23:00:27 -0300

    getStaticProps in Fixture

--- components/Fixture/Fixture.tsx
+++ components/Fixture/Fixture.tsx
@@ -18,6 +18,7 @@ import { AddMatchDto } from "../Admin/Fixture/interfaces";
 const STEP = 5;
 
 interface Props {
+  matchs: Match[];
   canDelete?: boolean;
   canEdit?: boolean;
 }
@@ -45,6 +46,17 @@ interface Match {
   rival_score?: number;
 }
 
+export async function getStaticProps() {
+  const res = await getMatchs(0, 50);
+  const matchs = res?.map((r) => ({ ...r, time: new Date(r.time) }));
+  return {
+    props: {
+      matchs,
+    },
+    revalidate: 30,
+  };
+}
+
 const FixtureImage = ({ source, alt, score }: FixtureImageProps) => {
   return (
     <div className="h-3/6 w-full m-auto md:p-4 bg-realwhite">
@@ -63,8 +75,8 @@ const FixtureImage = ({ source, alt, score }: FixtureImageProps) => {
   );
 };
 
-const Fixture = ({ canDelete, canEdit }: Props) => {
-  const [matchs, setMatchs] = useState<Match[]>([]);
+const Fixture = ({ canDelete, canEdit, matchs }: Props) => {
+  //const [matchs, setMatchs] = useState<Match[]>([]);
   const [nextAllowed, setNextAllowed] = useState(true);
   const [from, setFrom] = useState(0);
   const [to, setTo] = useState(STEP);
@@ -73,17 +85,17 @@ const Fixture = ({ canDelete, canEdit }: Props) => {
   const [editing, setEditing] = useState(false);
   const currentUser = useCurrentUser();
 
-  const getGames = async () => {
-    const res = await getMatchs(0, 50);
-    const auxRes = res?.map((r) => ({ ...r, time: new Date(r.time) }));
-    if (auxRes) {
-      setMatchs(auxRes);
-    }
-  };
+  // const getGames = async () => {
+  //   const res = await getMatchs(0, 50);
+  //   const auxRes = res?.map((r) => ({ ...r, time: new Date(r.time) }));
+  //   if (auxRes) {
+  //     setMatchs(auxRes);
+  //   }
+  // };
 
-  useEffect(() => {
-    getGames();
-  }, []);
+  // useEffect(() => {
+  //   getGames();
+  // }, []);
 
   const back = () => {
     setFrom(from - STEP);
@@ -111,7 +123,7 @@ const Fixture = ({ canDelete, canEdit }: Props) => {
       });
       const data = await res.json();
       alert("Partido borrado!");
-      getGames();
+      //getGames();
       return data;
     } catch (error) {
       console.error(error);
